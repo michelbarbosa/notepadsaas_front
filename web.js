f3a0bb33df7209fdaf5673e23fd3b93e609ffe3b
Heider Pinholi Lopes <heidertreinamentos@gmail.com>
2017-08-15 17:28:54 -0300

    Ajuste para o heroku

--- web.js
+++ web.js
@@ -1,4 +1,23 @@
 var express = require('express');
 var app = express();
-app.use(express.static(__dirname + '/app'));
-app.listen(process.env.PORT || 3000);
+
+// set the port of our application
+// process.env.PORT lets the port be set by Heroku
+var port = process.env.PORT || 8091;
+
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+// make express look in the public directory for assets (css/js/img)
+app.use(express.static(__dirname + '/'));
+
+// set the home page route
+app.get('/', function(req, res) {
+
+    // ejs render automatically looks in the views folder
+    res.render('index');
+});
+
+app.listen(port, function() {
+    console.log('Our app is running on http://localhost:' + port);
+});
