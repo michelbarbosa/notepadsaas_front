42bc8f31d3f5e60bf1a06a57e62be2773be7b130
Heider Pinholi Lopes <heidertreinamentos@gmail.com>
2017-08-15 17:41:21 -0300

    Ajuste no servico para chamar os webservices de prod

--- js/services/NotepadServices.js
+++ js/services/NotepadServices.js
@@ -1,9 +1,9 @@
-app.factory('notepadServices', ['$http', function($http) {
+app.factory('notepadServices', ['$http', 'locationServices', function($http, locationServices) {
 
         function listar(callback) {
             $http({
                 method:'GET',
-                url:'http://localhost:8090/nota'
+                url: 'https://notepadsaas.herokuapp.com/nota'
             }).then(function (data) {
                 if (callback) callback(data)
             });
@@ -12,7 +12,7 @@ app.factory('notepadServices', ['$http', function($http) {
         function pesquisarPor(nome, callback) {
             $http({
                 method:'GET',
-                url:'http://localhost:8090/nota/' + nome
+                url:'https://notepadsaas.herokuapp.com/nota/' + nome
             }).then(function (data) {
                 if (callback) callback(data)
             });
@@ -21,7 +21,7 @@ app.factory('notepadServices', ['$http', function($http) {
         function salvar(nota, callback) {
             $http({
                 method:'POST',
-                url:'http://localhost:8090/nota',
+                url:'https://notepadsaas.herokuapp.com/nota',
                 data:JSON.stringify(nota)
             }).then(function (data) {
                 if (callback) callback(data)
