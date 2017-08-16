83bc2645015a1d2f86598447f56c285c7d751b60
Michel Barbosa <michel.amb.92@gmail.com>
2017-08-15 22:44:07 -0300

    ajuste URL

--- js/services/NotepadServices.js
+++ js/services/NotepadServices.js
@@ -12,7 +12,7 @@ app.factory('notepadServices', ['$http', 'locationServices', function($http, loc
         function pesquisarPor(nome, callback) {
             $http({
                 method:'GET',
-                url:'https://notepad-saas-fiap.herokuapp.com/nota/' + nome
+                url:'https://notepad-saas-fiap.herokuapp.com/nota/titulo/' + nome
             }).then(function (data) {
                 if (callback) callback(data)
             });
